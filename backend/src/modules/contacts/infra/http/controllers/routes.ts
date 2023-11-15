6b08235b947750211215b73f72337a0dd97f36f5
CauaMatheus <caua10000@gmail.com>
2023-11-15 13:07:57 -0300

    fix: fixing permissions in reading contacts

--- backend/src/modules/contacts/infra/http/controllers/routes.ts
+++ backend/src/modules/contacts/infra/http/controllers/routes.ts
@@ -1,8 +1,18 @@
-import { FastifyInstance } from "fastify";
+import { FastifyInstance, FastifyPluginCallback } from "fastify";
+import { verifyJWT } from "../../../../../shared/middlewares/VerifyJWT";
 import { CreateContactsController } from "./CreateContactsController";
 import { ReadContactsController } from "./ReadContactsController";
 
 export async function contactsRoutes(app: FastifyInstance) {
   app.post("/", CreateContactsController);
-  app.get("/", ReadContactsController);
+
+  app.register(authenticatedRoutes);
 }
+
+const authenticatedRoutes: FastifyPluginCallback = (app, _, done) => {
+  app.addHook("onRequest", verifyJWT);
+
+  app.get("/", ReadContactsController);
+
+  done();
+};
