296dc8687989692a9831d77a4dda44e6f846986a
SamuelRicardoDS <samuelricardods@gmail.com>
2023-11-21 15:26:47 -0300

    fix(google-maps): fixing de map error

--- web/src/app/medias/page.tsx
+++ web/src/app/medias/page.tsx
@@ -5,7 +5,7 @@ import LayoutMap from "@/components/LayoutMap";
 export default function Medias() {
   return (
     <>
-      <LayoutMap  center={{lat: -15.832952, lng: -48.083647,}}/>
+      <LayoutMap center={{ lat: -15.832952, lng: -48.083647}} /> 
     </>
   );
 }
