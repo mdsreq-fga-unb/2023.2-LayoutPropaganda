5be1526fa1944a11d4afe099fc82983d814d5052
SamuelRicardoDS <samuelricardods@gmail.com>
2023-11-22 11:27:31 -0300

    feat(axios): start connection with api

--- web/src/app/login/page.tsx
+++ web/src/app/login/page.tsx
@@ -10,19 +10,52 @@ import {
   Label,
 } from "./styles";
 
+import { useState } from "react";
+import { api } from '../../services/api';
+
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {name, value} = e.target;
+    if(name === 'email') {
+      setEmail(value);
+    }
+    if(name === 'password') {
+      setPassword(value);
+    }
+  }
+  console.log(email);
+  console.log(password);
+
+
+  const handleSubmit = async(e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const data = {
+      email,
+      password
+    }
+    try {
+      const response = await api.post('/employees/authenticate', data);
+      console.log(response);
+    } catch (error) {
+      console.log(error); 
+      alert('Invalid email or password');
+    }
+  }
   return (
     <Container>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Fields>
           <Field>
             <Label>Email</Label>
-            <Input type="email" />
+            <Input type="email" name='email' onChange={handleChange}/>
           </Field>
 
           <Field>
             <Label>Senha</Label>
-            <Input type="password" />
+            <Input type="password" name='password' onChange={handleChange}/>
           </Field>
         </Fields>
         <Button type="submit">ENTRAR</Button>
