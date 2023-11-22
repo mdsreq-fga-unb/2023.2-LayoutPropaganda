7fc06e88ffc1ab6227bfa640dcecebcddb1ad17c
Vinicius-Muniz <vs454595@gmail.com>
2023-11-22 00:50:24 -0300

    Feat(Styled Components): list of contacts page prototype

--- web/src/app/clientes/page.tsx
+++ web/src/app/clientes/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Container, ContactListBox, ContactAtributeBox, ContactAtribute, ContactInfoBox } from "./styles"
+import { Container, ContactListBox, ContactAtributeBox, ContactAtribute, ContactInfoBox, ContactInfoLine, ContactAtributeName, ContactAtributeEmail, ContactAtributeTel, Checkbox } from "./styles"
 import React from "react";
 
 export default function Clients() {
@@ -8,6 +8,22 @@ export default function Clients() {
         { id: 2, nome: "Gustavo Henrique", email: "gugabalatensa@gmail.com", telefone: "(61) 98166-6912"},
         { id: 3, nome: "Samuel Ricardo", email: "samucards@gmail.com", telefone: "(61) 98186-2937"},
         { id: 4, nome: "Manoel Felipe", email: "pernalongacomu@gmail.com", telefone: "(61) 98129-8108"},
+        { id: 5, nome: "Gustavo Kenzo", email: "gkat@gmail.com", telefone: "(61) 98147-1212" },
+        { id: 6, nome: "Gustavo Henrique", email: "gugabalatensa@gmail.com", telefone: "(61) 98166-6912"},
+        { id: 7, nome: "Samuel Ricardo", email: "samucards@gmail.com", telefone: "(61) 98186-2937"},
+        { id: 8, nome: "Manoel Felipe", email: "pernalongacomu@gmail.com", telefone: "(61) 98129-8108"},
+        { id: 9, nome: "Gustavo Kenzo", email: "gkat@gmail.com", telefone: "(61) 98147-1212" },
+        { id: 10, nome: "Gustavo Henrique", email: "gugabalatensa@gmail.com", telefone: "(61) 98166-6912"},
+        { id: 11, nome: "Samuel Ricardo", email: "samucards@gmail.com", telefone: "(61) 98186-2937"},
+        { id: 12, nome: "Manoel Felipe", email: "pernalongacomu@gmail.com", telefone: "(61) 98129-8108"},
+        { id: 13, nome: "Gustavo Kenzo", email: "gkat@gmail.com", telefone: "(61) 98147-1212" },
+        { id: 14, nome: "Gustavo Henrique", email: "gugabalatensa@gmail.com", telefone: "(61) 98166-6912"},
+        { id: 15, nome: "Samuel Ricardo", email: "samucards@gmail.com", telefone: "(61) 98186-2937"},
+        { id: 16, nome: "Manoel Felipe", email: "pernalongacomu@gmail.com", telefone: "(61) 98129-8108"},
+        { id: 17, nome: "Gustavo Kenzo", email: "gkat@gmail.com", telefone: "(61) 98147-1212" },
+        { id: 18, nome: "Gustavo Henrique", email: "gugabalatensa@gmail.com", telefone: "(61) 98166-6912"},
+        { id: 19, nome: "Samuel Ricardo", email: "samucards@gmail.com", telefone: "(61) 98186-2937"},
+        { id: 20, nome: "Manoel Felipe", email: "pernalongacomu@gmail.com", telefone: "(61) 98129-8108"},
     ];
 
     return (
@@ -19,7 +35,16 @@ export default function Clients() {
                     <ContactAtribute>Telefone</ContactAtribute>
                 </ContactAtributeBox>
                 <ContactInfoBox>
-                    <div background-color="#000000">Teste</div>
+                    {contatos.map((contato) => (
+                        <div key={contato.id}>
+                            <ContactInfoLine>
+                                <Checkbox></Checkbox>
+                                <ContactAtributeName>{contato.nome}</ContactAtributeName>
+                                <ContactAtributeEmail>{contato.email}</ContactAtributeEmail>
+                                <ContactAtributeTel>{contato.telefone}</ContactAtributeTel>
+                            </ContactInfoLine>
+                        </div>
+                    ))}
                 </ContactInfoBox>
             </ContactListBox>
         </Container>
